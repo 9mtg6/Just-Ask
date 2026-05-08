466fb960eb08e861e100f173292fb85d48584d8f
Marwan Tarek Galal <marwan.tarek.abosalem@gmail.com>
2026-05-08 17:15:33 +0300

    ^_^

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,7 +14,7 @@ const teamMembers = [
   { name: 'Omar Elnagdy', role: 'Community Manager', image: '/Omar%20Elnagdy.jpeg' },
   { name: 'El-hussieen Ali', role: 'Co-Originator of the idea', image: '/El-hussieen%20Ali.jpeg' },
   { name: 'Ziad Mohamed', role: 'Marketing Team', image: '/Ziad%20Mohamed.jpeg' },  { name: 'Ziad Mohamed', role: 'Marketing Team', image: '/Ziad%20Mohamed.jpeg' },
-  { name: 'Anas Yasser', role: 'Originator of the idea', image: '/' },
+  { name: 'Anas Yasser', role: 'Originator of the idea', image: '/logo.png' },
 
 ]
 
